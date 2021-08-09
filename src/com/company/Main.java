a3715fc2b9706fde722e09f361609766d8a19a48
SandroHartmann <88325031+SandroHartmann@users.noreply.github.com>
2021-08-09 15:44:54 +0200

    Hello World

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
-
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wie hoch sollten die Blätter reichen?");
+
+        int blaetter = scanner.nextInt();
+
+
+
+    }
+    public static void blaetter(int blaetter){
+        for(int i= 0; i < blaetter; i++) {
+            new String(new char[i]).replace("\0", "*");
+            System.out.println(String());
+        }
     }
 }
